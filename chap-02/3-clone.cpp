f49aeea62581767fe00c150c0e5f239d5f0bfd94
Marc Agnetti <agnettim.pro@gmail.com>
2023-01-12 00:48:38 +0100

    Clone file done

--- chap-02/3-clone.cpp
+++ chap-02/3-clone.cpp
@@ -9,10 +9,31 @@ public:
         , _surname { surname }
     {}
 
+    Person(const Person& other)
+        : _name { other._name }
+        , _surname { other._surname }
+    {}
+
     std::string  get_full_name() const { return _name + " " + _surname; }
     unsigned int get_age() const { return _age; }
 
     void wait(unsigned int years) { _age += years; }
+
+    friend std::ostream& operator<<(std::ostream& stream, const Person& p)
+    {
+        return stream << "Person named '" << p.get_full_name() << "' is " << p.get_age() << " years old.";
+    }
+
+    Person& operator=(const Person& other)
+    {
+        if (this != &other)
+        {
+            _name    = other._name;
+            _surname = other._surname;
+        }
+
+        return *this;
+    }
 
 private:
     std::string  _name;
@@ -24,9 +45,16 @@ int main()
 {
     Person batman { "Bruce", "Wayne" };
     batman.wait(23);
+    std::cout << batman << std::endl;
 
-    std::cout << "Person named '" << batman.get_full_name() << "' is " << batman.get_age() << " years old."
-              << std::endl;
+    Person clone = batman;
+    std::cout << clone << std::endl;
+
+    Person assigned_clone { "Batman", "2" };
+    std::cout << assigned_clone << std::endl;
+
+    assigned_clone = batman;
+    std::cout << assigned_clone << std::endl;
 
     return 0;
 }
