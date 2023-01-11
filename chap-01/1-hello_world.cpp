1aa5dd238f13ed30315719b7615041eef87d9ac3
Marc Agnetti <agnettim.pro@gmail.com>
2023-01-11 09:06:53 +0100

    Parrot file updated

--- chap-01/1-hello_world.cpp
+++ chap-01/1-hello_world.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
-    std::cout << "Hi everyone!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    return 0;
 }
--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,40 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+
+    // int number = 0;
+    // std::string word;
+    string word;
+
+    // while (true)
+    // {
+    //     std::cin >> number;
+    //     if (number == -1)
+    //         break;
+    //     std::cout << "Craow " << number << " !" << std::endl;
+    // }
+
+    // do
+    // {
+    //     std::cin >> number;
+    //     std::cout << "Craow " << number << " !" << std::endl;
+    // }
+    // while (number != -1);
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     std::cin >> number;
+    //     std::cout << "Craow " << number << " !" << std::endl;
+    // }
+
+    // std::cin >> word;
+    // std::cout << "Craow " << word << "!" << std::endl;
+
+    cin >> word;
+    cout << "Craow " << word << "!" << endl;
+
+    return 0;
+}
