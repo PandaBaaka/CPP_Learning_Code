19d11d762a2b7b3909d692412d95bd2c69231420
Marc Agnetti <agnettim.pro@gmail.com>
2023-01-11 10:40:29 +0100

    Calculator

--- chap-01/4-calculator.cpp
+++ chap-01/4-calculator.cpp
@@ -1,66 +1,104 @@
-#include <iostream>
-#include <string>
-#include <vector>
+// #include <iostream>
+// #include <string>
+// #include <vector>
 
-int main(int argc, char** argv)
-{
-    // Parsing program parameters.
-    if (argc < 2)
-    {
-        std::cerr << "Expected operator as first argument." << std::endl;
-        return -1;
-    }
+// void display_result(int result)
+// {
+//     std::cout << "Result is " << result << std::endl;
+// }
 
-    std::string op = argv[1];
+// int add(const std::vector<int>& values)
+// {
+//     int result = 0;
+//     for (auto v : values)
+//     {
+//         result += v;
+//     }
+//     return result;
+// }
 
-    if (op != "+" && op != "*" && op != "-")
-    {
-        std::cerr << "Expected operator to be '+', '*' or '-'." << std::endl;
-        return -1;
-    }
+// int multiply(const std::vector<int>& values)
+// {
+//     auto result = 1;
+//     for (auto v : values)
+//     {
+//         result *= v;
+//     }
+//     return result;
+// }
 
-    std::vector<int> values;
-    for (auto arg_i = 2; arg_i < argc; ++arg_i)
-    {
-        auto value = std::stoi(argv[arg_i]);
-        values.emplace_back(value);
-    }
+// int sub(const std::vector<int>& values)
+// {
+//     auto result = values[0];
+//     for (auto i = 1; i < values.size(); ++i)
+//     {
+//         result -= values[i];
+//     }
+//     return result;
+// }
 
-    // Process operation, depending on the operator.
-    auto result = 0;
+// int compute_result(char op, const std::vector<int>& values)
+// {
+//     switch (op)
+//     {
+//     case '+':
+//         return add(values);
 
-    if (op == "+")
-    {
-        for (auto v : values)
-        {
-            result += v;
-        }
-    }
-    else if (op == "*")
-    {
-        result = 1;
-        for (auto v : values)
-        {
-            result *= v;
-        }
-    }
-    else if (op == "-")
-    {
-        if (values.empty())
-        {
-            std::cerr << "Operator '-' expects at least one operand to substract from." << std::endl;
-            return -1;
-        }
+//     case '*':
+//         return multiply(values);
 
-        result = values[0];
-        for (auto i = 1u; i < values.size(); ++i)
-        {
-            result -= values[i];
-        }
-    }
+//     case '-':
+//         return sub(values);
 
-    // Output result.
-    std::cout << "Result is " << result << std::endl;
+//     default:
+//         return 0;
+//     }
+// }
 
-    return 0;
-}
+// bool parse_params(char& op, std::vector<int>& values, int argc, char** argv)
+// {
+//     if (argc < 2)
+//     {
+//         std::cerr << "Expected operator as first argument." << std::endl;
+//         return false;
+//     }
+
+//     std::string op_str = argv[1];
+//     if (op_str != "+" && op_str != "*" && op_str != "-")
+//     {
+//         std::cerr << "Expected operator to be '+', '*' or '-'." << std::endl;
+//         return false;
+//     }
+
+//     op = op_str[0];
+
+//     for (auto arg_i = 2; arg_i < argc; ++arg_i)
+//     {
+//         auto value = std::stoi(argv[arg_i]);
+//         values.emplace_back(value);
+//     }
+
+//     if (op == '-' && values.empty())
+//     {
+//         std::cerr << "Operator '-' expects at least one operand to substract from." << std::endl;
+//         return -1;
+//     }
+
+//     return true;
+// }
+
+// int main(int argc, char** argv)
+// {
+//     char             op = '?';
+//     std::vector<int> values;
+
+//     if (!parse_params(op, values, argc, argv))
+//     {
+//         return -1;
+//     }
+
+//     auto result = compute_result(op, values);
+//     display_result(result);
+
+//     return 0;
+// }
