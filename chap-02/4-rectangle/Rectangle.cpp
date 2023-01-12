0a4f93c89750a2f897a17055c3cc265b7d9890d0
Marc Agnetti <agnettim.pro@gmail.com>
2023-01-12 01:48:27 +0100

    Rectangle file done

--- chap-02/4-rectangle/Rectangle.cpp
+++ chap-02/4-rectangle/Rectangle.cpp
@@ -1,1 +1,37 @@
-// Implement Rectangle functions here.
+#include "Rectangle.h"
+
+float Rectangle::_default_size = 0.f;
+
+Rectangle::Rectangle(float length, float width)
+    : _length { length }
+    , _width { width }
+{}
+
+// Rectangle::Rectangle(float size)
+//     : _length { size }
+//     , _width { size }
+// {}
+
+Rectangle::Rectangle(float size)
+    : Rectangle { size, size }
+{}
+
+Rectangle::Rectangle()
+    : Rectangle { _default_size }
+{}
+
+void Rectangle::scale(float ratio)
+{
+    _length *= ratio;
+    _width *= ratio;
+}
+
+void Rectangle::set_default_size(float size)
+{
+    _default_size = size;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Rectangle& r)
+{
+    return stream << "{ L: " << r.get_length() << ", W: " << r.get_width() << " }";
+}
--- chap-02/4-rectangle/Rectangle.h
+++ chap-02/4-rectangle/Rectangle.h
@@ -1,23 +1,37 @@
 #pragma once
+#include <ostream>
 
 class Rectangle
 {
 public:
-    Rectangle(float length, float width)
-        : _length { length }
-        , _width { width }
-    {}
+    // Rectangle(float length, float width)
+    //     : _length { length }
+    //     , _width { width }
+    // {}
+
+    // static float _default_size;
+
+    Rectangle(float length, float width);
+    Rectangle(float size);
+    Rectangle();
 
     float get_length() const { return _length; }
     float get_width() const { return _width; }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+    void scale(float ratio);
+
+    // void scale(float ratio)
+    // {
+    //     _length *= ratio;
+    //     _width *= ratio;
+    // }
+
+    static void set_default_size(float size);
+
+    friend std::ostream& operator<<(std::ostream& stream, const Rectangle& r);
 
 private:
-    float _length;
-    float _width;
+    float        _length;
+    float        _width;
+    static float _default_size;
 };
